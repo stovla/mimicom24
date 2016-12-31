a8adb56bc028dcc148cbec5e7d115af07bb30e80
Vlastimir <vlastimirsd@gmail.com>
2016-12-31 00:31:50 +0000

    add active navbar function, add hover efect

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     // ----- putting every button in paragraph to the right -----
     $('button').parent('p').css("text-align", "right");
+    
+    $('.nav li').click(function (e) {
+        var $this = $(this);
+        $('.nav li').removeClass('active');
+        if (!$this.hasClass('active')) {
+            $this.addClass('active');
+        }
+    });
 });
